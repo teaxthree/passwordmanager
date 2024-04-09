e700e1c7a83b88e025573c2947a52a38847bcdf6
teaxthree <tysonthetyrant@gmail.com>
2024-04-09 17:33:18 -0400

    Added encrypted key and json file to store user password info

--- password_manager.py
+++ password_manager.py
@@ -2,6 +2,7 @@ hardcoded_user = "abc" #placeholder for authentication
 hardcoded_password = "123" #placeholder for authentication
 user = "" #input of username
 pw = "" #input of password
+loginCounter = 3 #attempts remaining to input correct credentials
 
 def login(u, p):
     print("Invalid username or password.")
@@ -14,9 +15,13 @@ user = input("Username: ")
 pw = input("Password: ")
 
 #continuously prompt for login until valid credentials are provided
-while user != hardcoded_user and pw != hardcoded_password:
+while user != hardcoded_user and pw != hardcoded_password and loginCounter != 0:
+    print("Remaining login attempts: " + str(loginCounter))
     user, pw = login(user, pw)
+    loginCounter -= 1
+    
 
-print("Login successful.")
-
-     
+if (user == hardcoded_user) and (pw == hardcoded_password):
+    print("Login successful.")
+else:
+    print("Login unsuccessful")
